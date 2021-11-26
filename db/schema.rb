627a807b5ffd6d155be96872fbffe4a8a73c39e1
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2021-11-27 01:48:47 +0200

    Add blank task resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_234317) do
+ActiveRecord::Schema.define(version: 2021_11_26_234706) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name", null: false
@@ -19,4 +19,15 @@ ActiveRecord::Schema.define(version: 2021_11_26_234317) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.integer "project_id", null: false
+    t.string "title", null: false
+    t.integer "size", null: false
+    t.datetime "completed_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  add_foreign_key "tasks", "projects"
 end
