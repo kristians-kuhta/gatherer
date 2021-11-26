200ebcea4f7edc6f4106eab767c5488a7bec3d6b
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2021-11-26 17:46:45 +0200

    Add velocity calculation for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,26 @@
 class Task
   attr_reader :size
 
-  def initialize(size: 0, completed: false)
-    @completed = completed
+  def initialize(size: 0, completed_at: nil)
+    mark_as_completed(completed_at) if completed_at.present?
     @size = size
   end
 
   def completed?
-    @completed
+    @completed_at.present?
   end
 
-  def mark_as_completed
-    @completed = true
+  def mark_as_completed(date = Time.current)
+    @completed_at = date
+  end
+
+  def part_of_velocity?
+    return false unless @completed_at.present?
+
+    @completed_at > 21.days.ago
+  end
+
+  def points_toward_velocity
+    part_of_velocity? ? size : 0
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,12 +3,38 @@ require 'rails_helper'
 RSpec.describe Task do
   let(:task) { Task.new }
 
-  it 'does not have new task as completed' do
-    expect(task).not_to be_completed
+  describe 'completion' do
+    it 'does not have new task as completed' do
+      expect(task).not_to be_completed
+    end
+
+    it 'allows to complete a task' do
+      task.mark_as_completed
+      expect(task).to be_completed
+    end
   end
 
-  it 'allows to complete a task' do
-    task.mark_as_completed
-    expect(task).to be_completed
+  describe 'velocity' do
+    let(:task) { Task.new(size: 3) }
+
+    it 'does not include incomplete tasks when calculating velocity' do
+
+      expect(task).not_to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eq(0)
+    end
+
+    it 'includes recently completed tasks when calculating velocity' do
+      task.mark_as_completed(1.day.ago)
+
+      expect(task).to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eq(3)
+    end
+
+    it 'does not count a task that has been completed long-ago towards velocity' do
+      task.mark_as_completed(2.years.ago)
+
+      expect(task).not_to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eq(0)
+    end
   end
 end
